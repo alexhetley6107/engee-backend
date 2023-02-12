93441d79fa20f9e74e949ca083142e90c4b238c4
aleksSiemaG <your@email.com>
2023-02-12 15:35:51 +0200

    add configs n db

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
+dotenv.config();
+
+//Constants
+const PORT = process.env.PORT || 4000;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'hello from server OK#5' });
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.vbylrks.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    );
+
+    app.listen(PORT, () => console.log(`Server is OKK on ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
